58b207af9b26470ee8d749ebf7370364f825a5bf
chris92vr <garofolichristian@gmail.com>
2024-02-10 00:39:30 +0100

    Added Logout button

---
+++ components/LogoutButton.js
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const LogoutButton = () => {
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        // Remove the token from the cookie
+        document.cookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        // Redirect to the login page
+        window.location.href = '/login';
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <button type="submit" className="btn btn-link">
+                <FontAwesomeIcon icon="sign-out-alt" />
+                Logout
+            </button>
+        </form>
+    );
+}
+
+export default LogoutButton;
