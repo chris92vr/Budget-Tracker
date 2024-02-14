01da43cc5ae804023fa70936d2d98b88f8c82adf
chris92vr <garofolichristian@gmail.com>
2024-02-14 12:10:15 +0100

    Added social links

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,30 @@
-import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import styles from "@/styles/Footer.module.css";
-
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">
-                            <p>Home</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/about">
-                            <p>About</p>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <p>
+                            &copy; 2024 Budget Tracker
+                        </p>
+                        <div className={styles.socials}>
+                                <a href="github.com">
+                                <FontAwesomeIcon icon={faGithub} /> 
+                                </a>
+                          <a href="linkedin.com">
+                           
+                                <FontAwesomeIcon icon={faLinkedin}  />
+                                </a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
-
