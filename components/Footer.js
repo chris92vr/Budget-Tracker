57eb61ae26e73375af3a4a602632ae3d01ebc18b
chris92vr <garofolichristian@gmail.com>
2024-02-16 17:51:42 +0100

    Improved Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,30 +1,25 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import styles from "@/styles/Footer.module.css";
-import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import styles from '@/styles/Footer.module.css';
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 text-center">
-                        <p>
-                            &copy; 2024 Budget Tracker
-                        </p>
-                        <div className={styles.socials}>
-                                <a href="github.com">
-                                <FontAwesomeIcon icon={faGithub} /> 
-                                </a>
-                          <a href="linkedin.com">
-                           
-                                <FontAwesomeIcon icon={faLinkedin}  />
-                                </a>
-                            
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className={`${styles.footer} footer`}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <p>&copy; 2024 Budget Tracker</p>
+            <div className={styles.socials}>
+              <a href="github.com">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="linkedin.com">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
